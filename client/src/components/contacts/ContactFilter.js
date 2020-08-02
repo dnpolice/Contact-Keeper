4342cf539feab0a3c8174c6284bb6f26dc2e9d84
dnpolice <dylanpolicelli@gmail.com>
2020-08-02 18:17:18 -0400

    Front end created

---
+++ client/src/components/contacts/ContactFilter.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import ContactContext from '../../context/contact/contactContext';
+
+const ContactFilter = () => {
+    const contactContext = useContext(ContactContext);
+
+    const { filterContacts, clearFilter } = contactContext;
+
+    const onChange = e => {
+        if (e.target.value !== '') {
+            filterContacts(e.target.value);
+        } else {
+            clearFilter();
+        }
+    }
+
+
+    return (
+        <form>
+            <input type="text" placeholder="Filter Contacts..." onChange={onChange} />
+        </form>
+    )
+}
+
+export default ContactFilter
